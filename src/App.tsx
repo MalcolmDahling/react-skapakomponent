22678afd7b149bf5fdb1b242a64f77a64cd1a1fa
MalcolmDahling <mac.dahling@gmail.com>
2022-03-11 15:22:00 +0100

    done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Addition } from './components/addition/addition';
+import './components/addition/addition.css'
+
+function App() {
+    return (
+        <Addition/>
+    );
+}
+
+export default App;
---
+++ src/components/addition/addition.tsx
@@ -0,0 +1,11 @@
+export function Addition(){
+    return(
+        <div>
+            <input type="text"></input>
+            <p> + </p>
+            <input type="text"></input>
+            <p> = </p>
+            <span id="result"></span>
+        </div>
+    );
+}
